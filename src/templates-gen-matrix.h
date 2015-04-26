43c5a6452d3ff4f139c9a9b60143ba3c70443929
Dongcan Jiang <dongcan.jiang@gmail.com>
2015-04-26 20:26:29 +0800

    clean codes
    Signed-off-by: Dongcan Jiang <dongcan.jiang@gmail.com>

--- src/templates-gen-matrix.h
+++ src/templates-gen-matrix.h
@@ -5,9 +5,6 @@
 #define COLS_TYPE 'r'
 #include METHOD_TEMPLATE_H
 #undef COLS_TYPE
-//#define COLS_TYPE 'l'
-//#include METHOD_TEMPLATE_H
-//#undef COLS_TYPE
 
 #define ROWS_TYPE 'i'
 #include METHOD_TEMPLATE_H
@@ -17,9 +14,6 @@
 #define COLS_TYPE 'r'
 #include METHOD_TEMPLATE_H
 #undef COLS_TYPE
-//#define COLS_TYPE 'l'
-//#include METHOD_TEMPLATE_H
-//#undef COLS_TYPE
 #undef ROWS_TYPE
 
 #define ROWS_TYPE 'r'
@@ -30,23 +24,7 @@
 #define COLS_TYPE 'r'
 #include METHOD_TEMPLATE_H
 #undef COLS_TYPE
-//#define COLS_TYPE 'l'
-//#include METHOD_TEMPLATE_H
-//#undef COLS_TYPE
 #undef ROWS_TYPE
-
-//#define ROWS_TYPE 'l'
-//#include METHOD_TEMPLATE_H
-//#define COLS_TYPE 'i'
-//#include METHOD_TEMPLATE_H
-//#undef COLS_TYPE
-//#define COLS_TYPE 'r'
-//#include METHOD_TEMPLATE_H
-//#undef COLS_TYPE
-//#define COLS_TYPE 'l'
-//#include METHOD_TEMPLATE_H
-//#undef COLS_TYPE
-//#undef ROWS_TYPE
 
 RETURN_TYPE (*METHOD_NAME[3][3])(ARGUMENTS_LIST) = {
   {METHOD_NAME_noRows_noCols, METHOD_NAME_noRows_intCols, METHOD_NAME_noRows_realCols},
