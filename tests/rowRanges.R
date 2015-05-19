8b44ae5817dda29a7881c7a379eac3d39512a1dc
Dongcan Jiang <dongcan.jiang@gmail.com>
2015-05-19 09:28:28 +0800

    little changes to rowRanges tests
    Signed-off-by: Dongcan Jiang <dongcan.jiang@gmail.com>

--- tests/rowRanges.R
+++ tests/rowRanges.R
@@ -206,22 +206,14 @@ x <- matrix(runif(6*6, min=-6, max=6), nrow=6, ncol=6)
 storage.mode(x) <- "integer"
 for (rows in indexCases) {
   for (cols in indexCases) {
-    validateIndicesTestMatrix(x, rows, cols, ftest=rowRanges, fsure=rowRanges_R, na.rm=TRUE)
-    validateIndicesTestMatrix(x, rows, cols, ftest=rowRanges, fsure=rowRanges_R, na.rm=FALSE)
+    for (na.rm in c(TRUE, FALSE)) {
+      validateIndicesTestMatrix(x, rows, cols, ftest=rowRanges, fsure=rowRanges_R, na.rm=na.rm)
+      validateIndicesTestMatrix(x, rows, cols, ftest=rowMins, fsure=rowMins_R, na.rm=na.rm)
+      validateIndicesTestMatrix(x, rows, cols, ftest=rowMaxs, fsure=rowMaxs_R, na.rm=na.rm)
 
-    validateIndicesTestMatrix(x, rows, cols, ftest=rowMins, fsure=rowMins_R, na.rm=TRUE)
-    validateIndicesTestMatrix(x, rows, cols, ftest=rowMins, fsure=rowMins_R, na.rm=FALSE)
-
-    validateIndicesTestMatrix(x, rows, cols, ftest=rowMaxs, fsure=rowMaxs_R, na.rm=TRUE)
-    validateIndicesTestMatrix(x, rows, cols, ftest=rowMaxs, fsure=rowMaxs_R, na.rm=FALSE)
-
-    validateIndicesTestMatrix(x, rows, cols, fcolTest=colRanges, fsure=rowRanges_R, na.rm=TRUE)
-    validateIndicesTestMatrix(x, rows, cols, fcolTest=colRanges, fsure=rowRanges_R, na.rm=FALSE)
-
-    validateIndicesTestMatrix(x, rows, cols, fcolTest=colMins, fsure=rowMins_R, na.rm=TRUE)
-    validateIndicesTestMatrix(x, rows, cols, fcolTest=colMins, fsure=rowMins_R, na.rm=FALSE)
-
-    validateIndicesTestMatrix(x, rows, cols, fcolTest=colMaxs, fsure=rowMaxs_R, na.rm=TRUE)
-    validateIndicesTestMatrix(x, rows, cols, fcolTest=colMaxs, fsure=rowMaxs_R, na.rm=FALSE)
+      validateIndicesTestMatrix(x, rows, cols, fcolTest=colRanges, fsure=rowRanges_R, na.rm=na.rm)
+      validateIndicesTestMatrix(x, rows, cols, fcolTest=colMins, fsure=rowMins_R, na.rm=na.rm)
+      validateIndicesTestMatrix(x, rows, cols, fcolTest=colMaxs, fsure=rowMaxs_R, na.rm=na.rm)
+    }
   }
 }
